fad6f1439b764fa0482de8a42bff69d70fe81666
Rachit Bhargava <rachit@marble.io>
2019-09-19 14:29:14 -0700

    Adding output diag params to the marble config (#12)
    Using output diagnostic parameters

--- realsense2_camera/include/constants.h
+++ realsense2_camera/include/constants.h
@@ -66,13 +66,11 @@ namespace realsense2_camera
     const int ACCEL_FPS       = 1000;
     const int IMU_FPS         = 0;
 
-    const double DEPTH_FPS_TOLERANCE       = 0.1;
-    const double INFRA1_FPS_TOLERANCE      = 0.1;
-    const double INFRA2_FPS_TOLERANCE      = 0.1;
-    const double COLOR_FPS_TOLERANCE       = 0.1;
-    const double FISHEYE_FPS_TOLERANCE     = 0.1;
-    const double GYRO_FPS_TOLERANCE        = 0.001;
-    const double ACCEL_FPS_TOLERANCE       = 0.001;
+    const float DIAGNOSTIC_MIN_FREQUENCY          = 0.0;
+    const float DIAGNOSTIC_MAX_FREQUENCY          = 2000.0;
+    const float DIAGNOSTIC_MAX_INTERVAL_SEC       = 1.0;
+    const double DIAGNOSTIC_TIME_WINDOW_SEC        = 5.0;
+
 
     const bool ENABLE_DEPTH   = true;
     const bool ENABLE_INFRA1  = true;
